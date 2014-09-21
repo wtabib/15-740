6c21bd13721950e1118a4082728b71bfd72abde2
Vittorio Perera <vdperera@unix2.andrew.cmu.edu>
2014-09-21 18:50:17 -0400

    cache tool

---
+++ assignment1-handout/cache_tool.cpp
@@ -0,0 +1,51 @@
+#include "pin.H"
+
+#include <iostream>
+#include <fstream>
+
+#include "pin_profile.H"
+
+std::ofstream outFile;
+
+long long int counter = 0;
+
+VOID countInstruction()
+{
+	counter++;
+}
+
+VOID instruction(INS ins, void *v) 
+{
+	
+	INS_InsertPredicatedCall(
+			ins, IPOINT_BEFORE, (AFUNPTR) countInstruction,
+			IARG_END);
+
+}
+
+VOID fini(int code, VOID *v) 
+{
+	std::cout << "INSTRUCTION EXECUTION\t\t\t" << counter << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+	PIN_InitSymbols();
+
+	if( PIN_Init(argc,argv) )
+	{
+		std::cout << "you fucked up" << std::endl;
+	}
+
+
+	//outFile.open(KnobOutputFile.Value().c_str());
+
+	INS_AddInstrumentFunction(instruction, 0);
+	PIN_AddFiniFunction(fini, 0);
+
+	PIN_StartProgram();
+
+	return 0;
+}
+
+
