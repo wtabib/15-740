3db4f8c6807be9ee152bb89c9b51b7c0108b6ee3
Vittorio Perera <vdperera@cs.cmu.edu>
2014-11-03 17:43:29 -0500

    removed alias from compile

--- project/pin-2.14-67254-gcc.4.4.7-linux/source/tools/MyPinTool/compile.sh
+++ project/pin-2.14-67254-gcc.4.4.7-linux/source/tools/MyPinTool/compile.sh
@@ -8,4 +8,4 @@ rm ${PINEXEC}.so
 g++ -DBIGARRAY_MULTIPLIER=1 -g -Wall -Wno-unknown-pragmas -fno-stack-protector -DTARGET_IA32E -DHOST_IA32E -fPIC -DTARGET_LINUX -I../../../source/include/pin -I../../../source/include/pin/gen -I../../../extras/components/include -I../../../extras/xed2-intel64/include -I../../../source/tools/InstLib -O3 -fomit-frame-pointer -fno-strict-aliasing   -c -o ${PINEXEC}.o ${PINEXEC}.cpp
 g++ -shared -Wl,--hash-style=sysv -Wl,-Bsymbolic -Wl,--version-script=../../../source/include/pin/pintool.ver    -o  ${PINEXEC}.so ${PINEXEC}.o  -L../../../intel64/lib -L../../../intel64/lib-ext -L../../../intel64/runtime/glibc -L../../../extras/xed2-intel64/lib -lpin -lxed -ldwarf -lelf -ldl
 
-/home/vittorio/Downloads/pin-2.14-67254-gcc.4.4.7-linux/pin -injection child -t ${PINEXEC}.so -- ./$2.o
+pin -injection child -t ${PINEXEC}.so -- ./$2.o
